65f8635c41d1adf1758d31c72248c587b412d915
Ramsey Njire <ramseynjire@gmail.com>
2021-10-12 20:30:40 +0300

    write guessing_game#initialize method

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,3 +1,9 @@
-class GuessingGame
+require "byebug"
 
+class GuessingGame
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
 end
